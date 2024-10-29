a69606404132388935f9e8f9159428c17383e573
DanyCodex <daniel272aguilar@gmail.com>
2024-10-29 14:26:42 -0600

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: "https://danny-portfolio-5g45.vercel.app/",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
